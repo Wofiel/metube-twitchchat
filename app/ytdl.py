20d3a3aadbe50d617edb801a64816cff08db44aa
Wofiel <wofiel@wofiel.us>
2024-11-06 23:24:25 +0000

    Update ytdl.py wrap cmd

--- app/ytdl.py
+++ app/ytdl.py
@@ -109,7 +109,7 @@ class Download:
                "--generate-mask=true"
               ]
         print(cmd)
-        popen_and_call(tcd_render_post, cmd)
+        popen_and_call(tcd_render_post, (cmd,))
 
     def _download(self):
         try:
@@ -151,7 +151,7 @@ class Download:
                 self.info.tmp_render_location = f"{self.download_dir}/{self.info.title}_chat"
                 cmd = ["tcd", "chatdownload", f"{self.info.url}", "-o", f"{self.info.chat_location}", "--compression"]
                 print(cmd)
-                self.popen_and_call(self.tcd_dl_post,cmd)
+                self.popen_and_call(self.tcd_dl_post,(cmd,))
             self.status_queue.put({'status': 'finished' if ret == 0 else 'error'})
         except yt_dlp.utils.YoutubeDLError as exc:
             self.status_queue.put({'status': 'error', 'msg': str(exc)})
